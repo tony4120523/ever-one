6c27139f27aacf503b9176dd313d3e538af735aa
Hsu Tun <tony4120523@gmail.com>
2018-05-03 06:51:14 +0800

    new authentication

--- get_one.py
+++ get_one.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python 
 # -*- coding: utf-8 -*-
 #
-# A simple Evernote API demo script that lists all notebooks in the user's
-# account and creates a simple test note in the default notebook.
-#
 # Before running this sample, you must fill in your Evernote developer token.
-#
-# To run (Unix):
-#   export PYTHONPATH=../../lib; python EDAMTest.py
 #
 
 import random
 import hashlib
 import binascii
 import evernote.edam.userstore.constants as UserStoreConstants
-import evernote.edam.limits.constants as LimitConstants
+import lib.evernote.edam.limits.constants as LimitConstants
 import evernote.edam.type.ttypes as Types
 #import evernote.edam.notestore.NoteStore as NoteStore
 
@@ -36,26 +30,26 @@ from enml import PlainTextOfENML, HTMLOfENML
 # refer to https://dev.evernote.com/doc/articles/testing.php 
 # and https://dev.evernote.com/doc/articles/bootstrap.php
 
-auth_token = "S=s1:U=94930:E=16a74fe3f63:C=1631d4d1120:P=1cd:A=en-devtoken:V=2:H=bd72d309237af750c91c0d11cfa179d4"
+#auth_token = ""
 
-if auth_token == "your developer token":
-    print "Please fill in your developer token"
-    print "To get a developer token, visit " \
-        "https://sandbox.evernote.com/api/DeveloperToken.action"
-    exit(1)
+#if auth_token == "your developer token":
+#    print "Please fill in your developer token"
+#    print "To get a developer token, visit " \
+#        "https://sandbox.evernote.com/api/DeveloperToken.action"
+#    exit(1)
 
 
 # To access Sandbox service, set sandbox to True 
 # To access production (International) service, set both sandbox and china to False
 # To access production (China) service, set sandbox to False and china to True
-sandbox = True
-china = False
+#sandbox = True
+#china = False
 
 # Initial development is performed on our sandbox server. To use the production
 # service, change sandbox=False and replace your
 # developer token above with a token from
 # https://www.evernote.com/api/DeveloperToken.action
-client = EvernoteClient(token=auth_token, sandbox=sandbox, china=china)
+##client = EvernoteClient(token=auth_token, sandbox=sandbox, china=china)
 
 #user_store = client.get_user_store()
 
@@ -68,16 +62,41 @@ client = EvernoteClient(token=auth_token, sandbox=sandbox, china=china)
 #print ""
 #if not version_ok:
 #    exit(1)
+# ==============================================================
+## OAuth
+#client = EvernoteClient(
+#    consumer_key='',
+#    consumer_secret='',
+#    sandbox=True # Default: True
+#)
+#request_token = client.get_request_token('https://127.0.0.1:8000')
+#client.get_authorize_url(request_token)
+ 
+#To obtain the access token
+#request_token['aouth_verifier'] = "547E7EFCEBC38A4D966E72CB2AAE0D01&sandbox_lnb=false"
+#access_token = client.get_access_token(
+#    request_token['oauth_token'],
+#    request_token['oauth_token_secret'],
+#    request.GET.get('oauth_verifier', '')
+#    ['oauth_verifier', '547E7EFCEBC38A4D966E72CB2AAE0D01&sandbox_lnb=false']
+#)
 
+#client = EvernoteClient(token=access_token)
+#note_store = client.get_note_store()
+#notebooks = note_store.listNotebooks()
+#===================================================================
+#===================================================================
+
+# fill the token here
+auth_token = ""
+client = EvernoteClient(token=auth_token)
 note_store = client.get_note_store()
-
-# List all of the notebooks in the user's account
 notebooks = note_store.listNotebooks()
-#print "Found ", len(notebooks), " notebooks:"
-#for notebook in notebooks:
-#    print "  * ", notebook.name
+print "Found ", len(notebooks), " notebooks:"
+for notebook in notebooks:
+    print "  * ", notebook.name
 
-# write the notebook_name
+# fill the notebook_name
 notebook_name = "專案 1"
 notebook_guid = ""
 for notebook in notebooks:
@@ -88,7 +107,6 @@ filter.notebookGuid = notebook_guid
 spec = NoteStore.NotesMetadataResultSpec()
 spec.includeTitle = True
 spec.includeContentLength = True
-#spec.includeCreated = True
 note_infos = note_store.findNotesMetadata(auth_token, filter, 0,
                         LimitConstants.EDAM_USER_NOTES_MAX, spec)
 #print note_infos
